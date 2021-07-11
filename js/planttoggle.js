08ee42ae249ded99e0205b4f1cad9341c28a9426
C Long <clong389@gmail.com>
2021-07-11 15:43:24 -0400

    Problem! Tried to apply the toggle hide/show html, css, and js code to the other (3) tables on the Plants page. Only the bottom table is displaying on button click. Asking mentors to review.

--- js/planttoggle.js
+++ js/planttoggle.js
@@ -1,5 +1,32 @@
 function myFunction() {
-    var x = document.getElementById("plantToggle");
+    var x = document.getElementById("plantToggle1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("plantToggle2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("plantToggle3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("plantToggle4");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
