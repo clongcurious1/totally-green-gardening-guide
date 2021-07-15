f8a280daf5dc598ae6b3f4859447e129b4a8020c
C Long <clong389@gmail.com>
2021-07-14 20:12:44 -0400

    With help from CodeLou mentor and classmate, updated js function code (added param) and then modified Plants page HTML to pass plantToggle1 thru 4 into the appropriate onclick code. Plants page functionality is working now.

--- js/planttoggle.js
+++ js/planttoggle.js
@@ -1,35 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("plantToggle1");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+function myFunction(param) {
+    var x = document.getElementById(param);
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
   }
 
-  function myFunction() {
-    var x = document.getElementById("plantToggle2");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function myFunction() {
-    var x = document.getElementById("plantToggle3");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function myFunction() {
-    var x = document.getElementById("plantToggle4");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
