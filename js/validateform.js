5a03e6bb61603283d0165f111abeb646350df201
C Long <clong389@gmail.com>
2021-07-26 18:58:33 -0400

    Contact Form issue - I have two buttons. Top button, alerts and javascript are working. Issue 1: Form clears after successfully validating email format. Need form values to be kept until form is submitted. Submit button is not working at all. Will explore with help of mentor tonight.

--- js/validateform.js
+++ js/validateform.js
@@ -3,8 +3,7 @@ function ValidateEmail(inputText)
 var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 if(inputText.value.match(mailformat))
 {
-alert("Bravo! Your email address is formatted correctly. As this is only a demo site, none of your form data will be submitted to a server.");
-document.form1.text1.focus();
+alert("Bravo! Your email address is formatted correctly."); 
 return true;
 }
 else
@@ -15,3 +14,15 @@ return false;
 }
 }
 
+const form = document.getElementById('form');
+const thanks = document.getElementById('thanks');
+
+form.onsubmit = submit;
+
+function submit(event) {
+  form.setAttribute('hidden', '');
+  thanks.removeAttribute('hidden');
+
+  // For this example, don't actually submit the form
+  event.preventDefault();
+}
