6e143a2ec2cd916d883c7e7c8afadfa2ba8f6ed3
C Long <clong389@gmail.com>
2021-07-26 17:59:56 -0400

    Contact page - modified text in alerts inside validateform.js. May start over as validating formats is not all that we're supposed to do. We also need to have the page change (beyond alerts?) upon submit.

--- js/validateform.js
+++ js/validateform.js
@@ -3,13 +3,13 @@ function ValidateEmail(inputText)
 var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 if(inputText.value.match(mailformat))
 {
-alert("Bravo! Your email address is valid!");
+alert("Bravo! Your email address is formatted correctly. As this is only a demo site, none of your form data will be submitted to a server.");
 document.form1.text1.focus();
 return true;
 }
 else
 {
-alert("Try again! You entered an invalid email address!");
+alert("Try again! A valid email address is required to submit this form.");
 document.form1.text1.focus();
 return false;
 }
